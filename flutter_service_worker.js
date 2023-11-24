ec1d194104002e0aaf4eb085b24bfca93c0c441f
NishchalVerma <nishiverma82@gmail.com>
2023-11-24 15:55:56 +0530

    initial commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "f63910dd23e28020ccf041f4d8f2c15c",
-"index.html": "a11607691da43aa4db47098de63326c4",
-"/": "a11607691da43aa4db47098de63326c4",
-"main.dart.js": "34badd03a39b46845035e70f033927e7",
+"index.html": "081bbaca787c755c3f33a502459ae28d",
+"/": "081bbaca787c755c3f33a502459ae28d",
+"main.dart.js": "4257f536282f23beaa9d423c0e6a38ec",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
