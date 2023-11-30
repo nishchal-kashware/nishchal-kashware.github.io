a357a1099c25564fd3e02bf7158ea2b0a6354c50
NishchalVerma <nishiverma82@gmail.com>
2023-11-30 11:56:43 +0530

    design changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "f63910dd23e28020ccf041f4d8f2c15c",
-"index.html": "081bbaca787c755c3f33a502459ae28d",
-"/": "081bbaca787c755c3f33a502459ae28d",
-"main.dart.js": "4257f536282f23beaa9d423c0e6a38ec",
+"index.html": "947bc889aa98f90f6c6bbbbb4dd13e8b",
+"/": "947bc889aa98f90f6c6bbbbb4dd13e8b",
+"main.dart.js": "ef94a96296ce74b82bdbb2053b2931bf",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
